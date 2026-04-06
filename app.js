acb0293795c3c7478d0941cd285f527e233a533a
saniyaahmed123 <saniya2012ahmed@gmail.com>
2026-04-06 14:26:18 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,435 @@
+var celesteStore = {
+    women: {
+        nike: {
+            t_shirts: {
+                nike_w_casa_batllo: {
+                    title: "Casa Batllo Graphic Tee",
+                    brand: "Nike",
+                    gender: "women",
+                    img: "assets/tshirts/t-shirt3.jpg",
+                    hoverImg: "assets/tshirts/t-shirt3-hover.jpg",
+                    price: 35.90,
+                    desc: "Oversized cotton t-shirt featuring a sketch of Gaudí’s Casa Batlló."
+                },
+                nike_w_legend_tee: {
+                    title: "Striped Baggy Tee",
+                    brand: "Nike",
+                    gender: "women",
+                    img: "assets/tshirts/p4.jpg",
+                    hoverImg: "assets/tshirts/hover.jpg",
+                    price: 25.00,
+                    desc: "Soft cotton performance shirt."
+                },
+                nike_w_strike_pro: {
+                    title: "Strike Pro Pink Tee",
+                    brand: "Nike",
+                    gender: "women",
+                    img: "assets/tshirts/red.jpg",
+                    hoverImg: "assets/tshirts/redhover.jpg",
+                    price: 35.00,
+                    desc: "Tight-fit gym base layer."
+                },
+                nike_w_black_long: {
+                    title: "Black Long Tee",
+                    brand: "Nike",
+                    gender: "women",
+                    img: "assets/tshirts/longblack.jpg",
+                    hoverImg: "assets/tshirts/longblackhover.jpg",
+                    price: 35.00,
+                    desc: "Tight-fit gym base layer."
+                }
+            },
+            pants: {
+                nike_w_blue_wide_jeans: {
+                    title: "Blue Wide Jeans",
+                    brand: "Nike",
+                    gender: "women",
+                    img: "assets/pants/pant1.webp",
+                    hoverImg: "assets/pants/pant1hover.webp",
+                    price: 40.00,
+                    desc: "Stretchable training shorts."
+                },
+                nike_w_black_flat_jeans: {
+                    title: "Black Flat Jeans",
+                    brand: "Nike",
+                    gender: "women",
+                    img: "assets/pants/pant2.webp",
+                    hoverImg: "assets/pants/pant2hover.webp",
+                    price: 40.00,
+                    desc: "Stretchable training shorts."
+                },
+                nike_w_printed_white_jeans: {
+                    title: "Printed White Jeans",
+                    brand: "Nike",
+                    gender: "women",
+                    img: "assets/pants/printed white hover.jpg",
+                    hoverImg: "assets/pants/printed white jeans.jpg",
+                    price: 32.00,
+                    desc: "Stretchable training shorts."
+                },
+                nike_w_check_trouser: {
+                    title: "Check Trouser",
+                    brand: "Nike",
+                    gender: "women",
+                    img: "assets/pants/checker.jpg",
+                    hoverImg: "assets/pants/checkerhover.jpg",
+                    price: 47.00,
+                    desc: "Stretchable training shorts."
+                },
+                nike_w_blue_flat_jeans: {
+                    title: "Blue Flat Jeans",
+                    brand: "Nike",
+                    gender: "women",
+                    img: "assets/pants/pant3.webp",
+                    hoverImg: "assets/pants/pant3hover.webp",
+                    price: 47.00,
+                    desc: "Stretchable training shorts."
+                }
+            },
+            dress: {
+                nike_w_gingham_dress: {
+                    title: "Gingham Shirt Dress",
+                    brand: "Nike",
+                    gender: "women",
+                    img: "assets/dress/d1nh.jpg",
+                    hoverImg: "assets/dress/d1.jpg",
+                    price: 33.00,
+                    desc: "Dark Buttoned Shirt."
+                }
+            }
+        },
+        adidas: {
+            shirts: {
+                adidas_w_lavender_puffed: {
+                    title: "Lavender Puffed Shirt",
+                    brand: "Adidas",
+                    gender: "women",
+                    img: "assets/shirts/lavendershirt.jpg",
+                    hoverImg: "assets/shirts/lavendershirthover.jpg",
+                    price: 33.00,
+                    desc: "Dark Buttoned Shirt."
+                },
+                adidas_w_cream_striped: {
+                    title: "Cream Striped Shirt",
+                    brand: "Adidas",
+                    gender: "women",
+                    img: "assets/shirts/stripsshirt.jpg",
+                    hoverImg: "assets/shirts/stripsshirthover.jpg",
+                    price: 25.00,
+                    desc: "Wide sleeves shirt."
+                },
+                adidas_w_formal_stretch: {
+                    title: "Formal Stretch Shirt",
+                    brand: "Adidas",
+                    gender: "women",
+                    img: "assets/shirts/blackshirt.png",
+                    hoverImg: "assets/shirts/blackshirthover.png",
+                    price: 45.00,
+                    desc: "Premium stretch formal shirt."
+                }
+            },
+            coat_and_cardigan: {
+                adidas_w_trench_coat: {
+                    title: "Blue Trench Coat",
+                    brand: "Adidas",
+                    gender: "women",
+                    img: "assets/coat/c1.jpg",
+                    hoverImg: "assets/coat/c1h.jpg",
+                    price: 80.00,
+                    desc: "Iconic blue trench coat."
+                },
+                adidas_w_adicolor_hoodie: {
+                    title: "Adicolor Hoodie",
+                    brand: "Adidas",
+                    gender: "women",
+                    img: "assets/coat/c2.jpg",
+                    hoverImg: "assets/coat/c2h.jpg",
+                    price: 65.00,
+                    desc: "Classic trefoil fleece hoodie."
+                }
+            },
+            pants: {
+                adidas_w_tiro_pants: {
+                    title: "Tiro Tapered Pants",
+                    brand: "Adidas",
+                    gender: "women",
+                    img: "assets/pants/p1.jpg",
+                    hoverImg: "assets/pants/p1h.jpg",
+                    price: 45.00,
+                    desc: "Classic soccer tapered fit."
+                }
+            },
+            dress: {
+                adidas_w_button_down: {
+                    title: "Button Downed Shirt Dress",
+                    brand: "Adidas",
+                    gender: "women",
+                    img: "assets/dress/d4.jpg",
+                    hoverImg: "assets/dress/d4h.jpg",
+                    price: 33.00,
+                    desc: "Dark Buttoned Shirt."
+                },
+                adidas_w_check_dress: {
+                    title: "Check Shirt Dress",
+                    brand: "Adidas",
+                    gender: "women",
+                    img: "assets/dress/d5.jpg",
+                    hoverImg: "assets/dress/d5h.jpg",
+                    price: 33.00,
+                    desc: "Dark Buttoned Shirt."
+                }
+            }
+        },
+        zara: {
+            dress: {
+                zara_w_check_dress: {
+                    title: "Check Shirt Dress",
+                    brand: "Zara",
+                    gender: "women",
+                    img: "assets/dress/d2h.jpg",
+                    hoverImg: "assets/dress/d2.jpg",
+                    price: 33.00,
+                    desc: "Dark Buttoned Shirt."
+                },
+                zara_w_midi_dress: {
+                    title: "Midi Dress",
+                    brand: "Zara",
+                    gender: "women",
+                    img: "assets/dress/d3.jpg",
+                    hoverImg: "assets/dress/d3h.jpg",
+                    price: 33.00,
+                    desc: "Dark Buttoned Shirt."
+                }
+            },
+            shirts: {
+                zara_w_black_sweat: {
+                    title: "Black Sweatshirt",
+                    brand: "Zara",
+                    gender: "women",
+                    img: "assets/shirts/blacksweat.webp",
+                    hoverImg: "assets/shirts/blacksweathover.webp",
+                    price: 33.00,
+                    desc: "Dark Buttoned Shirt."
+                },
+                zara_w_brown_shirt: {
+                    title: "Dark Brown Shirt",
+                    brand: "Zara",
+                    gender: "women",
+                    img: "assets/shirts/brownshirt.jpg",
+                    hoverImg: "assets/shirts/brownshirthover.jpg",
+                    price: 25.00,
+                    desc: "Wide sleeves shirt."
+                },
+                zara_w_formal_stretch: {
+                    title: "Formal Stretch Shirt",
+                    brand: "Zara",
+                    gender: "women",
+                    img: "assets/shirts/blackshirt.png",
+                    hoverImg: "assets/shirts/blackshirthover.png",
+                    price: 45.00,
+                    desc: "Premium stretch formal shirt."
+                }
+            },
+            pants: {
+                zara_w_tiro_pants: {
+                    title: "Tiro Tapered Pants",
+                    brand: "Zara",
+                    gender: "women",
+                    img: "assets/pants/p2.jpg",
+                    hoverImg: "assets/pants/p2h.jpg",
+                    price: 45.00,
+                    desc: "Classic soccer tapered fit."
+                }
+            }
+        }
+    },
+    men: {
+        nike: {
+            t_shirts: {
+                nike_m_dri_fit: {
+                    title: "Dri-FIT Academy Tee",
+                    brand: "Nike",
+                    gender: "men",
+                    img: "assets/tshirts/m1.jpg",
+                    hoverImg: "assets/tshirts/m1h.jpg",
+                    price: 30.00,
+                    desc: "Sweat-wicking fabric for intense workouts."
+                },
+                nike_m_sportswear_club: {
+                    title: "Sportswear Club Tee",
+                    brand: "Nike",
+                    gender: "men",
+                    img: "assets/tshirts/m2.jpg",
+                    hoverImg: "assets/tshirts/m2h.jpg",
+                    price: 25.00,
+                    desc: "Classic everyday cotton tee."
+                }
+            },
+            pants: {
+                nike_m_flex_shorts: {
+                    title: "Flex Training Shorts",
+                    brand: "Nike",
+                    gender: "men",
+                    img: "assets/tshirts/m3.jpg",
+                    hoverImg: "assets/tshirts/m1h.jpg",
+                    price: 35.00,
+                    desc: "Made to move during high-intensity intervals."
+                }
+            }
+        },
+        adidas: {
+            shirts: {
+                adidas_m_trefoil_shirt: {
+                    title: "Trefoil Series Shirt",
+                    brand: "Adidas",
+                    gender: "men",
+                    img: "assets/m4.jpg",
+                    hoverImg: "assets/m4h.jpg",
+                    price: 28.00,
+                    desc: "Vintage inspired graphic shirt."
+                }
+            },
+            outerwear: {
+                adidas_m_tiro_jacket: {
+                    title: "Tiro Track Jacket",
+                    brand: "Adidas",
+                    gender: "men",
+                    img: "assets/coat/m1.jpg",
+                    hoverImg: "assets/coat/m1h.jpg",
+                    price: 60.00,
+                    desc: "Iconic soccer-style track top."
+                },
+                  zara_m_leather_jacket: {
+                    title: "Faux Leather Jacket",
+                    brand: "Zara",
+                    gender: "men",
+                    img: "assets/coat/m4.jpg",
+                    hoverImg: "assets/coat/m4h.jpg",
+                    price: 89.00,
+                    desc: "Sleek biker-style outerwear."
+                }
+            }
+        },
+        zara: {
+            shirts: {
+                zara_m_textured_relaxed: {
+                    title: "Textured Relaxed Shirt",
+                    brand: "Zara",
+                    gender: "men",
+                    img: "assets/shirts/m2.jpg",
+                    hoverImg: "assets/shirts/m2h.jpg",
+                    price: 45.90,
+                    desc: "Breathable fabric with a modern fit."
+                },
+                zara_m_linen_blend: {
+                    title: "Linen Blend Shirt",
+                    brand: "Zara",
+                    gender: "men",
+                    img: "assets/shirts/m3.jpg",
+                    hoverImg: "assets/shirts/m3h.jpg",
+                    price: 49.00,
+                    desc: "Lightweight summer essential."
+                }
+            },
+            pants: {
+                zara_m_slim_chinos: {
+                    title: "Slim Fit Chinos",
+                    brand: "Zara",
+                    gender: "men",
+                    img: "assets/pants/m1.jpg",
+                    hoverImg: "assets/pants/m1h.jpg",
+                    price: 59.00,
+                    desc: "Versatile trousers for work or play."
+                },
+                zara_m_cargo_joggers_v1: {
+                    title: "Cargo Joggers (Version A)",
+                    brand: "Zara",
+                    gender: "men",
+                    img: "assets/pants/m2.jpg",
+                    hoverImg: "assets/pants/m2h.jpg",
+                    price: 65.00,
+                    desc: "Utility style with comfortable elastic cuffs."
+                },
+                zara_m_cargo_joggers_v2: {
+                    title: "Cargo Joggers (Version B)",
+                    brand: "Zara",
+                    gender: "men",
+                    img: "assets/pants/m3.jpg",
+                    hoverImg: "assets/pants/m3h.jpg",
+                    price: 65.00,
+                    desc: "Utility style with comfortable elastic cuffs."
+                }
+            },
+            outerwear: {
+                zara_m_leather_jacket: {
+                    title: "Faux Leather Jacket",
+                    brand: "Zara",
+                    gender: "men",
+                    img: "assets/coat/m2.jpg",
+                    hoverImg: "assets/coat/m2h.jpg",
+                    price: 89.00,
+                    desc: "Sleek biker-style outerwear."
+                },
+                zara_m_leather_jacket: {
+                    title: "Faux Leather Jacket",
+                    brand: "Zara",
+                    gender: "men",
+                    img: "assets/coat/m3.jpg",
+                    hoverImg: "assets/coat/m3h.jpg",
+                    price: 89.00,
+                    desc: "Sleek biker-style outerwear."
+                },
+              
+            }
+        }
+    }
+};
+function readNestedObj(obj) {
+    for (var key in obj) {
+        if (typeof obj[key] === "object" && obj[key] !== null) {
+            if (obj[key].img) {
+                renderCard(key, obj[key]);
+            } else {
+                readNestedObj(obj[key]);
+            }
+        }
+    }
+}
+
+var productsGrid = document.getElementById('productsGrid');
+
+function displayAllPro(obj) {
+    for (var key in obj) {
+        var value = obj[key];
+        if (typeof value === "object" && value !== null) {
+            if (value.price) {productsGrid.innerHTML += `
+                <div class="card">
+                    <div class="card-img-top">
+                        <img src="${value.img}" class="product-img">
+                        <img src="${value.hoverImg}" class="hover-img base-img">
+                    </div>
+                    <div class="card-body d-flex flex-column justify-content-between mt-0">
+                        <p class="brand-name-text" style="color: gray; font-size: 0.8rem; margin-bottom: 2px; text-transform: uppercase;">
+                            ${value.brand}
+                        </p>
+                        
+                        <h5 class="card-title">${value.title}</h5>
+                        
+                        <p class="card-text text-muted" style="font-size: 0.85rem;">
+                            ${value.gender}'s Collection
+                        </p>
+                        
+                        <p class="card-text"><strong>$${value.price.toFixed(2)}</strong></p>
+                        <button class="btn">buy now</button>
+                    </div>
+                </div>`;
+            }
+            else {
+                displayAllPro(value);
+            }
+        }
+
+    }
+
+}
+displayAllPro(celesteStore);
